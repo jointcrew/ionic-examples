e3ec49c1f2e1ced1c1fb8a838f0c947d9e659a65
Takeshi_Emoto <takeshi.emoto@jointcrew.co.jp>
2019-12-08 23:20:27 +0900

    feat(wp-tutorial): 詳細画面の実装まで

--- wp-tutorial/src/app/home/home.page.ts
+++ wp-tutorial/src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  posts: {
+    ID: number;
+    title: string;
+    content: string;
+    date: string;
+  }[] = [];
 
-  constructor() {}
+  constructor(private http: HttpClient, public loadingController: LoadingController) {
+  }
+
+  async ionViewDidEnter() {
+    const loading = await this.loadingController.create({
+      message: 'Loading...'
+    });
+    await loading.present();
+    this.http.get('https://public-api.wordpress.com/rest/v1.1/sites/ionicjp.wordpress.com/posts').subscribe(data => {
+      this.posts = data['posts'];
+      loading.dismiss();
+    });
+  }
 
 }
