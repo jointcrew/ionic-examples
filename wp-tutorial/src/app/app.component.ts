0e8c4e8507f20bd646ab60c23bf2c5c389105085
Takeshi_Emoto <takeshi.emoto@jointcrew.co.jp>
2019-12-09 22:49:18 +0900

    feat(wp-tutorial): Google Analyticsタグ埋め込み

--- wp-tutorial/src/app/app.component.ts
+++ wp-tutorial/src/app/app.component.ts
@@ -1,21 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private router: Router
   ) {
     this.initializeApp();
+  }
+
+  ngOnInit(): void {
+    this.router.events.subscribe(params => {
+      if (params instanceof NavigationEnd && !this.platform.is('hybrid')) {
+        gtag('config', 'UA-154216863-1', { page_path: params.url });
+      }
+    })
   }
 
   initializeApp() {
